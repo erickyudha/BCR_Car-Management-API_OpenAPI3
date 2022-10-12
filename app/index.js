e142a80ec17dd11bd1930d8cf0937ec1233432c2
Erick Yudha <erickyudha18@gmail.com>
2022-10-12 10:52:44 +0700

    setup car model, service, repository

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const router = require("../config/routes");
+const fs = require('fs')
+const path = require("path")
+const cors = require('cors');
 
 const app = express();
 
-/** Install request logger */
-app.use(morgan("dev"));
+app.use(cors({
+    origin: '*'
+}));
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+app.use(morgan("dev"))
 
 /** Install JSON request parser */
 app.use(express.json());
