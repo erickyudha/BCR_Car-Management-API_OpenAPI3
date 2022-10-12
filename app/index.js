a86539881cde65b2d1309913e00357060b1c74b9
Erick Yudha <erickyudha18@gmail.com>
2022-10-12 10:36:53 +0700

    service repository project setup

---
+++ app/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const router = require("../config/routes");
+
+const app = express();
+
+/** Install request logger */
+app.use(morgan("dev"));
+
+/** Install JSON request parser */
+app.use(express.json());
+
+/** Install Router */
+// app.use(router);
+
+app.get("", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "Oii"
+    })
+})
+
+module.exports = app;
