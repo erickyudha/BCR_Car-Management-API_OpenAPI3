b562a579e507181518db615fb91f1effaa4b4fe2
Erick Yudha <erickyudha18@gmail.com>
2022-10-18 18:28:57 +0700

    remove timestamp from image and user model

--- app/models/image.js
+++ app/models/image.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Image',
+    timestamps: false,
   });
   return Image;
 };
--- db/migrations/20221017115248-create-image.js
+++ db/migrations/20221017115248-create-image.js
@@ -14,14 +14,6 @@ module.exports = {
       },
       url: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
