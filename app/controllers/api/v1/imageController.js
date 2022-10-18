306ca805c04572533e81e12909da70279da2351b
Erick Yudha <erickyudha18@gmail.com>
2022-10-18 20:40:52 +0700

    add missing image field handler

--- app/controllers/api/v1/imageController.js
+++ app/controllers/api/v1/imageController.js
@@ -2,27 +2,34 @@ const imageService = require("../../../services/imageService");
 
 module.exports = {
     upload(req, res) {
-        const fileBase64 = req.file.buffer.toString("base64");
-        const file = `data:${req.file.mimetype};base64,${fileBase64}`;
-        imageService.upload(file)
-            .then(result => {
-                res.status(201).json({
-                    status: "success",
-                    message: "Upload image successfully",
-                    data: {
-                        id: result.id,
-                        url: result.url,
-                        public_id: result.public_id
-                    }
-                });
+        try {
+            const fileBase64 = req.file.buffer.toString("base64");
+            const file = `data:${req.file.mimetype};base64,${fileBase64}`;
+            imageService.upload(file)
+                .then(result => {
+                    res.status(201).json({
+                        status: "success",
+                        message: "Upload image successfully",
+                        data: {
+                            id: result.id,
+                            url: result.url,
+                            public_id: result.public_id
+                        }
+                    });
+                })
+                .catch(err => {
+                    res.status(500)
+                        .json({
+                            status: "error",
+                            message: err.message
+                        })
+                })
+        } catch (error) {
+            res.status(422).json({
+                status: "failed",
+                message: "Missing required fields"
             })
-            .catch(err => {
-                res.status(500)
-                    .json({
-                        status: "error",
-                        message: err.message
-                    })
-            })
+        }
     },
 
     async delete(req, res) {
