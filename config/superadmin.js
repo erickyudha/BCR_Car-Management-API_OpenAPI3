d4c3acd42be68283ecc4c8301b70cda1cf21681c
Erick Yudha <erickyudha18@gmail.com>
2022-10-19 19:21:30 +0700

    minor fix

--- config/superadmin.js
+++ config/superadmin.js
@@ -2,7 +2,7 @@
 // * Need to reseed database with sequelize to take effect
 
 module.exports = {
-    name: "Mr.Super Admin",
+    name: "Mr.Superadmin",
     email: "super@admin.com",
     password: "admin"
 }
